3e5034c9c841ebf181cf417dfe419d331ce4042e
Pourab Karchaudhuri <36023@Hexaware.com>
2018-04-19 12:05:31 +0530

    Up

--- brain.py
+++ brain.py
@@ -1,8 +1,9 @@
 import requests
 
 # url = "https://lh138x41ek.execute-api.us-east-1.amazonaws.com/brain"
-url = "https://gtdfrjtay1.execute-api.us-east-1.amazonaws.com/proxy"
-
+url = "https://intense-cliffs-66388.herokuapp.com/brain"
+# payload = {"lang": "en","query": "Hello","sessionId": "12345","timezone":"Asia/Colombo"}
+# payload = str(payload)
 payload = "{\r\n    \"lang\": \"en\",\r\n    \"query\": \"Hello\",\r\n    \"sessionId\": \"12345\",\r\n    \"timezone\": \"Asia/Colombo\"\r\n  }"
 headers = {
     'Content-Type': "application/json",
